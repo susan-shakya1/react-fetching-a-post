c1493d3c2f8755f6c51639569ff57b43725a1760
susan-shakya1 <susanshakya2059@gmail.com>
2024-02-19 19:31:37 +0545

    styleing a content

--- src/components/postcard.tsx
+++ src/components/postcard.tsx
@@ -1,7 +1,20 @@
 export function Postcard(props: { title: string; description: string }) {
   return (
-    <div>
-      <h1>{props.title}</h1>
+    <div
+      style={{
+        maxWidth: "600px",
+        margin: "15px auto",
+        border: "1px solid #aaa",
+        padding: "15px",
+      }}
+    >
+      <h1
+        style={{
+          color: "#9e9e9e",
+        }}
+      >
+        {props.title}
+      </h1>
       <p>{props.description}</p>
     </div>
   );
